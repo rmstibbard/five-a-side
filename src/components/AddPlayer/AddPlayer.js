3a13a029fa10f849662ba138937dd14d413a067a
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-24 17:36:03 +0100

    All player form - First attempt

--- src/components/AddPlayer/AddPlayer.js
+++ src/components/AddPlayer/AddPlayer.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+class AddPlayer extends Component {
 
-function AddPlayer() {
-  return (
-    <div>Add Player</div>
-  );
-}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerName: props.playerName,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  handleChange(e) {
+    this.setState({ playerName: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleSubmit({ ...this.state });
+  }
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <h2 className="form-header">Enter a player's name</h2>
+
+        <form className="entry-form" onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} />
+          <button type="submit">Add a player</button>
+        </form>
+        {console.log(this.state)}
+      </React.Fragment>
+    )
+
+  }
+};
 
 export default AddPlayer;
