8f9b501aabe511e67c744d0ad44e3515a77a45e4
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-24 17:41:03 +0100

    Container file 'src/components/AddPlayer/index.js' for AddPlayer component

---
+++ src/components/AddPlayer/index.js
@@ -0,0 +1,17 @@
+import { connect } from "react-redux";
+import AddPlayer from "./AddPlayer";
+import { addPlayer } from "../../data/actions";
+
+const mapStateToProps = (state) => {
+  return {
+    playerName: state.playerName,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleSubmit: (data) => dispatch(addPlayer(data)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddPlayer);
