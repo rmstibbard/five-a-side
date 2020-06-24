e0c3728edd6754bc73fa317b214b187a8817e49d
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-24 19:16:07 +0100

    Added shufflePlayers function to 'reducer.js'

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -5,9 +5,21 @@ const addPlayer = (state, action) => {
   }
 };
 
+
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+const shufflePlayers = (state) => {
+  return {
+    ...state,
+    playersList: [
+      ...state.playersList.sort(() => Math.random() - 0.5)
+    ]
+  }
+}
+
+
 const reducer = (state, action) => {
   switch (action.type) {
-    case "ADD_PLAYER": return addPlayer(state, action);
+    case "ADD_PLAYER": return shufflePlayers(addPlayer(state, action));
     default: return state;
   }
 };
