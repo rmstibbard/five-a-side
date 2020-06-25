ef2654a698d4ab0a0bdf3e2018caa470854f4fdd
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-25 12:11:54 +0100

    Styling

--- src/components/DisplayTeams/DisplayTeams.js
+++ src/components/DisplayTeams/DisplayTeams.js
@@ -2,21 +2,26 @@ import React from "react";
 
 const DisplayTeams = ({ teamA, teamB }) => (
   <React.Fragment>
-    <div>
-      <p>TeamA</p>
-      <ul>
-        {teamA.map((player, index) => (
-          <li key={index}>{player.name}</li>
-        ))}
-      </ul>
-    </div>
-    <div>
-      <p>TeamB</p>
-      <ul>
-        {teamB.map((player, index) => (
-          <li key={index}>{player.name}</li>
-        ))}
-      </ul>
+    <div className="game">
+      <div className="team-a">
+        <p>TeamA</p>
+        <ul className="team-a-players">
+          {teamA.map((player, index) => (
+            <li key={index}>{player.name}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <div className="team-b">
+          <p>TeamB</p>
+          <ul className="team-b-players">
+            {teamB.map((player, index) => (
+              <li key={index}>{player.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   </React.Fragment>
 );
