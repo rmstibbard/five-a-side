b566fb4dc43ac5537822a67050601aab51004273
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-26 10:40:26 +0100

    Updates

---
+++ precache-manifest.67f0eeb6f72457d9a4ad3dd9b0ae112c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d2244ace29abacf15eb8a2c3cc3d82",
+    "url": "/five-a-side/index.html"
+  },
+  {
+    "revision": "cb0f512d18146f1b1463",
+    "url": "/five-a-side/static/css/main.a87954e6.chunk.css"
+  },
+  {
+    "revision": "103b79a299569f587de9",
+    "url": "/five-a-side/static/js/2.b1d2bff4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/five-a-side/static/js/2.b1d2bff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb0f512d18146f1b1463",
+    "url": "/five-a-side/static/js/main.fda65248.chunk.js"
+  },
+  {
+    "revision": "4218b05688c2a021ea4f",
+    "url": "/five-a-side/static/js/runtime-main.f5fc50a0.js"
+  },
+  {
+    "revision": "af3d166e9297c38c44958c1f11c249bb",
+    "url": "/five-a-side/static/media/background.af3d166e.jpg"
+  },
+  {
+    "revision": "096751397ac2fb9022ead49fc914f920",
+    "url": "/five-a-side/static/media/pitch.09675139.jpg"
+  }
+]);
