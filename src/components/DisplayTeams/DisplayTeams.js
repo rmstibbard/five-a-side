997675e751aca2e70ab4331dc65592245f9ea911
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-26 16:08:42 +0100

    Teams only display once sorted

--- src/components/DisplayTeams/DisplayTeams.js
+++ src/components/DisplayTeams/DisplayTeams.js
@@ -4,46 +4,43 @@ const DisplayTeams = ({ playersList, teamA, teamB }) => (
   <React.Fragment>
     <div className="game">
 
-      <div>
-        {playersList.length < 10 ?
-          <div className="players-list-title">
-            <p>Players to sort</p>
-            <ul className="players-list">
-              {
-                playersList.map((player, index) => (
-                  <li key={index}>{player.name}</li>
-                ))}
-            </ul>
-          </div> :
-          null
-        }
+      {playersList.length < 10 ?
+        <div className="players-list-title">
+          <p>Players to sort</p>
+          <ul className="players-list">
+            {
+              playersList.map((player, index) => (
+                <li key={index}>{player.name}</li>
+              ))}
+          </ul>
+        </div> :
+        null
+      }
 
+      {playersList.length >= 10 ?
         <div className="team-a-title">
           <p>Team A</p>
           <ul className="team-a-players">
-            {playersList.length >= 10 ?
-              teamA.map((player, index) => (
-                <li key={index}>{player.name}</li>
-              )) :
-              <p>Thinking ...</p>
-            }
+            {teamA.map((player, index) => (
+              <li key={index}>{player.name}</li>
+            ))}
           </ul>
-        </div>
+        </div> :
+        null
+      }
 
-        <div>
-          <div className="team-b-title">
-            <p>Team B</p>
-            <ul className="team-b-players">
-              {playersList.length >= 10 ?
-                teamB.map((player, index) => (
-                  <li key={index}>{player.name}</li>
-                )) :
-                <p>Thinking ...</p>
-              }
-            </ul>
-          </div>
-        </div>
-      </div>
+      {playersList.length >= 10 ?
+        <div className="team-b-title">
+          <p>Team B</p>
+          <ul className="team-b-players">
+            {teamA.map((player, index) => (
+              <li key={index}>{player.name}</li>
+            ))}
+          </ul>
+        </div> :
+        null
+      }
+
     </div>
   </React.Fragment>
 );
