9bfde14b20c1078f5daace7e35b132a2fc66edf1
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-26 22:41:49 +0100

    Added Redux developer tools

--- src/data/store.js
+++ src/data/store.js
@@ -1,10 +1,13 @@
 
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import reducer from './reducer';
 import initialState from './initialState';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer,
   initialState,
+  composeEnhancers()
 );
 export default store;
